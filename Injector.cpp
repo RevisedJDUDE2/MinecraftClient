1b01e4ba95e9bc3b4ad937bf29f549318545977c
RevisedJDUDE2 <JDUDEofficial134@gmail.com>
2024-01-28 18:33:24 +0800

    Add project files.

---
+++ Injector.cpp
@@ -0,0 +1,16 @@
+#include "Injector.h"
+
+Returns FindProcessByName(const char* Name, lpProcesss process) {
+  PROCESSENTRY32 proc;
+  HANDLE procHandle;
+  procHandle = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+  if (procHandle == INVALID_HANDLE_VALUE) {
+    process->processHandle = nullptr;
+    return Returns::Error_unexpected;
+  };
+  proc.dwSize = sizeof(PROCESSENTRY32);
+  if (!Process32First(procHandle, &proc)) {
+    process->processHandle = nullptr;
+    return Returns::Invalid;
+  }
+}
---
+++ Injector.h
@@ -0,0 +1,18 @@
+﻿#include <Windows.h>
+#include <iostream>
+#include <TlHelp32.h>
+
+typedef struct tagProcess {
+  std::string processName;
+  unsigned int processId;
+  HANDLE processHandle;
+} Process, * lpProcesss;
+
+enum class Returns {
+  Invalid,
+  Error_unexpected,
+  process_quited,
+  unknown
+};
+
+Returns FindProcessByName(const char* Name, lpProcesss process);
