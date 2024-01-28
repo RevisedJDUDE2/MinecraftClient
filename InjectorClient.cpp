901833f4be7116571545f971a843b68fb4addec1
RevisedJDUDE2 <JDUDEofficial134@gmail.com>
2024-01-28 20:26:50 +0800

    .

--- InjectorClient.cpp
+++ InjectorClient.cpp
@@ -3,7 +3,9 @@
 
 int main() {
   Process MinecraftProcess;
-  FindProcessByName("Minecraft.windows.exe", &MinecraftProcess);
+  if (FindProcessByName("Minecraft.windows.exe", &MinecraftProcess) == (Returns::Error_unexpected || Returns::Invalid)) {
+
+  }
   HANDLE McHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, MinecraftProcess.processId);
   return 0;
 }
